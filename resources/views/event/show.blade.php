87b0d2b5f6d87fc8196f935adf0dcee26568f560
affanhabib <ahsanulhabib11@gmail.com>
2022-07-22 06:30:33 +0700

    update link daftar event

--- resources/views/event/show.blade.php
+++ resources/views/event/show.blade.php
@@ -43,7 +43,7 @@
             @if($event->tanggal_berakhir < date('Y-m-d'))
             <a type="button" class="btn btn-success" disabled target="_blank">Daftar Event</a>
             @else
-            {{ $link = "//$event->link" }}
+            {{ $link = "http://$event->link" }}
             <a type="button" class="btn btn-success" href="{{ $link }}" target="_blank">Daftar Event</a>
             @endif
             <a type="button" class="btn btn-outline-success ml-3" href="http://wa.me/6282340576107 " target="_blank">Hubungi Kami</a>
